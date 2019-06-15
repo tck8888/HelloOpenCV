526e386f5aee0f1a4db8b5f1d1ec571b9286096c
tck8888 <380054728@qq.com>
2019-06-15 18:07:05 +0800

    vs配置cmake

---
+++ HelloOpenCVInCmake/HelloOpenCVInCmake/HelloOpenCVInCmake.cpp
@@ -0,0 +1,19 @@
+﻿// HelloOpenCVInCmake.cpp: 定义应用程序的入口点。
+//
+
+#include "HelloOpenCVInCmake.h"
+#include <opencv2/opencv.hpp>
+using namespace std;
+using namespace cv;
+int main()
+{
+	VideoCapture capture(0);
+	Mat img;
+	while (1)
+	{
+		capture >> img;
+		imshow("摄像头", img);
+		waitKey(10);
+	}
+	return 0;
+}
