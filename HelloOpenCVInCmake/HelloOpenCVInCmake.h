c5aef4004dab05f43fa909113bb5ae4308d4b29f
tck8888 <380054728@qq.com>
2019-06-18 00:09:42 +0800

    人脸定位

--- HelloOpenCVInCmake/HelloOpenCVInCmake.h
+++ HelloOpenCVInCmake/HelloOpenCVInCmake.h
@@ -4,5 +4,34 @@
 #pragma once
 
 #include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+class CascadeDetectorAdapter : public DetectionBasedTracker::IDetector
+{
+public:
+	CascadeDetectorAdapter(cv::Ptr<cv::CascadeClassifier> detector) :
+		IDetector(),
+		Detector(detector) {
+
+		CV_Assert(detector);
+	}
+
+	void detect(const cv::Mat& Image, std::vector<cv::Rect>& objects)
+	{
+
+		Detector->detectMultiScale(Image, objects, scaleFactor, minNeighbours, 0, minObjSize, maxObjSize);
+
+	}
+
+	virtual ~CascadeDetectorAdapter()
+	{
+
+	}
+
+private:
+	CascadeDetectorAdapter();
+	cv::Ptr<cv::CascadeClassifier> Detector;
+};
+
 
 // TODO: 在此处引用程序需要的其他标头。
